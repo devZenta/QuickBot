b2069dc39b9fafcca0cbcb74b69d3b48c7025a9f
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-03-14 14:47:33 +0100

    initialize project structure with basic files and configurations

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Bot: require('./src/Bot'),
+    Command: require('./src/commands/Command'),
+    CommandHandler: require('./src/handlers/commandHandler'),
+    EventHandler: require('./src/handlers/eventHandler'),
+    Event: require('./src/events/Event'),
+    Embed: require('./src/utils/Embed'),
+}
